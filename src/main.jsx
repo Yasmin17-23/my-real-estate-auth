45f3f4c51ef1532b96b5c7204bbf707026949498
Yasmin17-23 <zyasmins1747@gmail.com>
2024-08-14 21:33:11 +0600

    created images for assets folder and created basic layout and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
